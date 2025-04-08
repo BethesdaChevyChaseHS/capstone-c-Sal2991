973321fbe064c61cfddb0e489776a14130b51b76
Sal2991 <180361633+Sal2991@users.noreply.github.com>
2025-04-08 12:04:50 +0000

    save

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int FtoC(int degree){
+    return (degree - 32) * 5/9;
+}
+
+int main()
+{
+    int input;
+    std::cout << "Enter a Fahrenheit: ";
+    std::cin >> input;
+    std::cout << "In Celcius It's: " <<  FtoC(input) << std::endl;
+
     return 0;
 }
